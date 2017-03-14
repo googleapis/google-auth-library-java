2acb2d9ead0d94bcd8f6e24763bbeec3862d918c
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-03-15 12:49:55 +1300

    Minor cleanup removing unnecessary imports and suppress warnings

--- oauth2_http/java/com/google/auth/oauth2/DefaultCredentialsProvider.java
+++ oauth2_http/java/com/google/auth/oauth2/DefaultCredentialsProvider.java
@@ -38,12 +38,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.AccessControlException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Locale;
 
