761a192eeaa1fa1467c2a382128b6ef60af9ed60
Jeff Ching <chingor@google.com>
2018-10-01 22:15:09 -0700

    Fix release directory

--- .kokoro/release/stage.sh
+++ .kokoro/release/stage.sh
@@ -16,8 +16,8 @@
 set -eo pipefail
 
 source $(dirname "$0")/common.sh
-MAVEN_SETTINGS_FILE=$(realpath $(dirname "$0")/../)/settings.xml
-pushd $(dirname "$0")/../
+MAVEN_SETTINGS_FILE=$(realpath $(dirname "$0")/../../)/settings.xml
+pushd $(dirname "$0")/../../
 
 setup_environment_secrets
 create_settings_xml_file "settings.xml"
