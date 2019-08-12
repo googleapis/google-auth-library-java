adb01fcd3a1a56bc8a7fa4517c549faaace9ee7b
salmaan rashid <salrashid123@gmail.com>
2019-08-12 16:13:17 -0700

    add copyright

--- oauth2_http/javatests/com/google/auth/oauth2/IdTokenCredentialsTest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/IdTokenCredentialsTest.java
@@ -1,5 +1,5 @@
 /*
- * 2019 Google LLC
+ * Copyright 2019 Google LLC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
