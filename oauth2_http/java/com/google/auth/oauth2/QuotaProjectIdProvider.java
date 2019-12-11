bf9c02a6c47438592a1ce6d56164d4b4c1da948e
Cody Oss <codyoss@google.com>
2019-12-11 09:35:31 -0700

    fix formatting

--- oauth2_http/java/com/google/auth/oauth2/QuotaProjectIdProvider.java
+++ oauth2_http/java/com/google/auth/oauth2/QuotaProjectIdProvider.java
@@ -31,12 +31,8 @@
 
 package com.google.auth.oauth2;
 
-/**
- * Interface for {@link GoogleCredentials} that return a quota project ID.
- */
+/** Interface for {@link GoogleCredentials} that return a quota project ID. */
 public interface QuotaProjectIdProvider {
-    /**
-     * @return the quota project ID used for quota and billing purposes
-     */
-    String getQuotaProjectId();
+  /** @return the quota project ID used for quota and billing purposes */
+  String getQuotaProjectId();
 }
