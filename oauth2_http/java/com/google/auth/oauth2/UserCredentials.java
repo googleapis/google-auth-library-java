195eba54a9b3fc4e4a64c2d1d38898a0b2be9f9b
Guillaume Blaquiere <guillaume.blaquiere@gmail.com>
2020-10-12 22:40:36 +0200

    fix: clean unused import.

--- oauth2_http/java/com/google/auth/oauth2/UserCredentials.java
+++ oauth2_http/java/com/google/auth/oauth2/UserCredentials.java
@@ -58,6 +58,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+
 /** OAuth2 Credentials representing a user's identity and consent. */
 public class UserCredentials extends GoogleCredentials
     implements QuotaProjectIdProvider, IdTokenProvider {
