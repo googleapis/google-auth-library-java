dc59126a4cfba74f87d1cc21e9c52020d068bbc1
Leonardo Siracusa <leosiracusa@google.com>
2020-12-11 16:11:35 -0800

    fix: remove TODOs

--- oauth2_http/java/com/google/auth/oauth2/StsRequestHandler.java
+++ oauth2_http/java/com/google/auth/oauth2/StsRequestHandler.java
@@ -51,17 +51,12 @@ import javax.annotation.Nullable;
 
 /**
  * Implements the OAuth 2.0 token exchange based on https://tools.ietf.org/html/rfc8693.
- *
- * <p>TODO(lsirac): Add client auth support. TODO(lsirac): Ensure request content is formatted
- * correctly.
  */
 public class StsRequestHandler {
   private static final String TOKEN_EXCHANGE_GRANT_TYPE =
       "urn:ietf:params:oauth:grant-type:token-exchange";
   private static final String REQUESTED_TOKEN_TYPE =
       "urn:ietf:params:oauth:token-type:access_token";
-  private static final String CLOUD_PLATFORM_SCOPE =
-      "https://www.googleapis.com/auth/cloud-platform";
   private static final String PARSE_ERROR_PREFIX = "Error parsing token response.";
 
   private String tokenExchangeEndpoint;
