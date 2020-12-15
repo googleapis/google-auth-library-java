f5adc8cfaed63465b1e93bf77d8bf8b95656ffff
Leonardo Siracusa <leosiracusa@google.com>
2020-12-14 16:44:11 -0800

    fix: lint

--- oauth2_http/java/com/google/auth/oauth2/StsRequestHandler.java
+++ oauth2_http/java/com/google/auth/oauth2/StsRequestHandler.java
@@ -49,9 +49,7 @@ import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Nullable;
 
-/**
- * Implements the OAuth 2.0 token exchange based on https://tools.ietf.org/html/rfc8693.
- */
+/** Implements the OAuth 2.0 token exchange based on https://tools.ietf.org/html/rfc8693. */
 public class StsRequestHandler {
   private static final String TOKEN_EXCHANGE_GRANT_TYPE =
       "urn:ietf:params:oauth:grant-type:token-exchange";
