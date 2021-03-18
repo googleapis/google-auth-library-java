061448209da05ddfc75b40aae495c33d0ee7f1ee
arithmetic1728 <58957152+arithmetic1728@users.noreply.github.com>
2021-03-18 13:58:00 -0700

    fix: fix service account credentials createScopedRequired (#601)
    * fix: fix service account credentials createScopedRequired
    
    * Update oauth2_http/javatests/com/google/auth/oauth2/ServiceAccountCredentialsTest.java

--- oauth2_http/javatests/com/google/auth/oauth2/ServiceAccountCredentialsTest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/ServiceAccountCredentialsTest.java
@@ -479,12 +479,12 @@ public class ServiceAccountCredentialsTest extends BaseSerializationTest {
   }
 
   @Test
-  public void createScopedRequired_emptyScopes_false() throws IOException {
+  public void createScopedRequired_emptyScopes() throws IOException {
     GoogleCredentials credentials =
         ServiceAccountCredentials.fromPkcs8(
             CLIENT_ID, CLIENT_EMAIL, PRIVATE_KEY_PKCS8, PRIVATE_KEY_ID, EMPTY_SCOPES);
 
-    assertFalse(credentials.createScopedRequired());
+    assertTrue(credentials.createScopedRequired());
   }
 
   @Test
