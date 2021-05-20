e3f4c7eac0417705553ef8259599ec29fc8ad9b4
Igor Bernstein <igorbernstein@google.com>
2021-05-20 10:24:45 -0400

    feat: enable pre-emptive async oauth token refreshes (#646)
    * feat: add pre-emptive async oauth token refreshes
    
    This is currently a rough sketch and should not be merged. I just wanted to get some feedback here.
    
    The current implementation of oauth refresh offloads the IO to a separate executor, however when a token expires, all calls to getRequestMetadata would hang until the token is refreshed.
    This PR is a rough sketch to improve the situation by adding a stale state to token. If a token is within a minute of expiration, the first request to notice this, will spawn a refresh on the executor and immediately return with the existing token. This avoids hourly latency spikes for grpc.
    
    The implementation uses guava's ListenableFutures to manage the async refresh state. Although the apis are marked BetaApi in guava 19, they are GA in guava 30
    
    * The class introduces 3 distinct states:
      * Expired -  the token is not usable
      * Stale - the token is useable, but its time to refresh
      * Fresh - token can be used without any extra effort
    * All of the funtionality of getRequestMetadata has been extracted to asyncFetch. The new function will check if the token is unfresh and schedule a refresh using the executor
    * asyncFetch uses ListenableFutures to wrap state: if the token is not expired, an immediate future is returned. If the token is expired the future of the refresh task is returned
    * A helper refreshAsync 	& finishRefreshAsync are also introduced. They schedule the actual refresh and propagate the side effects
    * To handle blocking invocation: the async functionality is re-used but a DirectExecutor is used. All ExecutionErrors are unwrapped. In most cases the stack trace will be preserved because of the DirectExecutor. However if the async & sync methods are interleaved, it's possible that a sync call will await an async refresh task. In this case the callers stacktrace will not be present.
    
    * minor doc
    
    * update broken test
    
    * prep for merging with master: The initial async refresh feature was implemented on top of 0.8, so now I'm backporting features to minimize the merge conflicts
    
    * in blocking mode, when a token is stale, only block the first caller and allow subsequent callers to use the stale token
    
    * use private monitor to minimize change noise
    
    * minor tweaks and test
    
    * format
    
    * fix ExternalAccountCredentials
    
    * fix boundary checks and add a few more tests
    
    * add another test for making sure that blocking stacktraces include the caller
    
    * address feedback
    
    * optimize for the common case
    
    * codestyle
    
    * use Date to calculate cache state to fix tests that mock access token
    
    * remove accidental double call
    
    Co-authored-by: Les Vogel <lesv@users.noreply.github.com>

--- oauth2_http/java/com/google/auth/oauth2/OAuth2Credentials.java
+++ oauth2_http/java/com/google/auth/oauth2/OAuth2Credentials.java
@@ -31,37 +31,50 @@
 
 package com.google.auth.oauth2;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+
 import com.google.api.client.util.Clock;
 import com.google.auth.Credentials;
 import com.google.auth.RequestMetadataCallback;
 import com.google.auth.http.AuthHttpConstants;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableFutureTask;
+import com.google.common.util.concurrent.MoreExecutors;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.ServiceLoader;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
+import javax.annotation.Nullable;
 
 /** Base type for Credentials using OAuth2. */
 public class OAuth2Credentials extends Credentials {
 
   private static final long serialVersionUID = 4556936364828217687L;
-  private static final long MINIMUM_TOKEN_MILLISECONDS = 60000L * 5L;
-  private static final Map<String, List<String>> EMPTY_EXTRA_HEADERS = Collections.emptyMap();
+  static final long MINIMUM_TOKEN_MILLISECONDS = MINUTES.toMillis(5);
+  static final long REFRESH_MARGIN_MILLISECONDS = MINIMUM_TOKEN_MILLISECONDS + MINUTES.toMillis(1);
+  private static final ImmutableMap<String, List<String>> EMPTY_EXTRA_HEADERS = ImmutableMap.of();
 
   // byte[] is serializable, so the lock variable can be final
-  private final Object lock = new byte[0];
-  private Map<String, List<String>> requestMetadata;
-  private AccessToken temporaryAccess;
+  @VisibleForTesting final Object lock = new byte[0];
+  private volatile OAuthValue value = null;
+  @VisibleForTesting transient ListenableFutureTask<OAuthValue> refreshTask;
 
   // Change listeners are not serialized
   private transient List<CredentialsChangedListener> changeListeners;
@@ -90,7 +103,7 @@ public class OAuth2Credentials extends Credentials {
    */
   protected OAuth2Credentials(AccessToken accessToken) {
     if (accessToken != null) {
-      useAccessToken(accessToken, EMPTY_EXTRA_HEADERS);
+      this.value = OAuthValue.create(accessToken, EMPTY_EXTRA_HEADERS);
     }
   }
 
@@ -117,25 +130,21 @@ public class OAuth2Credentials extends Credentials {
    * @return The cached access token.
    */
   public final AccessToken getAccessToken() {
-    return temporaryAccess;
+    OAuthValue localState = value;
+    if (localState != null) {
+      return localState.temporaryAccess;
+    }
+    return null;
   }
 
   @Override
   public void getRequestMetadata(
       final URI uri, Executor executor, final RequestMetadataCallback callback) {
-    Map<String, List<String>> metadata;
-    synchronized (lock) {
-      if (shouldRefresh()) {
-        // The base class implementation will do a blocking get in the executor.
-        super.getRequestMetadata(uri, executor, callback);
-        return;
-      }
-      if (requestMetadata == null) {
-        throw new NullPointerException("cached requestMetadata");
-      }
-      metadata = requestMetadata;
-    }
-    callback.onSuccess(metadata);
+
+    Futures.addCallback(
+        asyncFetch(executor),
+        new FutureCallbackToMetadataCallbackAdapter(callback),
+        MoreExecutors.directExecutor());
   }
 
   /**
@@ -144,43 +153,18 @@ public class OAuth2Credentials extends Credentials {
    */
   @Override
   public Map<String, List<String>> getRequestMetadata(URI uri) throws IOException {
-    synchronized (lock) {
-      if (shouldRefresh()) {
-        refresh();
-      }
-      if (requestMetadata == null) {
-        throw new NullPointerException("requestMetadata");
-      }
-      return requestMetadata;
-    }
-  }
-
-  /** Refresh the token by discarding the cached token and metadata and requesting the new ones. */
-  @Override
-  public void refresh() throws IOException {
-    synchronized (lock) {
-      requestMetadata = null;
-      temporaryAccess = null;
-      AccessToken accessToken = refreshAccessToken();
-      if (accessToken == null) {
-        throw new NullPointerException("new access token");
-      }
-      useAccessToken(accessToken, getAdditionalHeaders());
-      if (changeListeners != null) {
-        for (CredentialsChangedListener listener : changeListeners) {
-          listener.onChanged(this);
-        }
-      }
-    }
+    return unwrapDirectFuture(asyncFetch(MoreExecutors.directExecutor())).requestMetadata;
   }
 
   /**
-   * Provide additional headers to return as request metadata.
-   *
-   * @return additional headers
+   * Request a new token regardless of the current token state. If the current token is not expired,
+   * it will still be returned during the refresh.
    */
-  protected Map<String, List<String>> getAdditionalHeaders() {
-    return EMPTY_EXTRA_HEADERS;
+  @Override
+  public void refresh() throws IOException {
+    AsyncRefreshResult refreshResult = getOrCreateRefreshTask();
+    refreshResult.executeIfNew(MoreExecutors.directExecutor());
+    unwrapDirectFuture(refreshResult.task);
   }
 
   /**
@@ -189,30 +173,168 @@ public class OAuth2Credentials extends Credentials {
    * @throws IOException during token refresh.
    */
   public void refreshIfExpired() throws IOException {
+    // asyncFetch will ensure that the token is refreshed
+    unwrapDirectFuture(asyncFetch(MoreExecutors.directExecutor()));
+  }
+
+  /**
+   * Attempts to get a fresh token.
+   *
+   * <p>If a fresh token is already available, it will be immediately returned. Otherwise a refresh
+   * will be scheduled using the passed in executor. While a token is being freshed, a stale value
+   * will be returned.
+   */
+  private ListenableFuture<OAuthValue> asyncFetch(Executor executor) {
+    AsyncRefreshResult refreshResult = null;
+
+    // fast and common path: skip the lock if the token is fresh
+    // The inherent race condition here is a non-issue: even if the value gets replaced after the
+    // state check, the new token will still be fresh.
+    if (getState() == CacheState.FRESH) {
+      return Futures.immediateFuture(value);
+    }
+
+    // Schedule a refresh as necessary
     synchronized (lock) {
-      if (shouldRefresh()) {
-        refresh();
+      if (getState() != CacheState.FRESH) {
+        refreshResult = getOrCreateRefreshTask();
+      }
+    }
+    // Execute the refresh if necessary. This should be done outside of the lock to avoid blocking
+    // metadata requests during a stale refresh.
+    if (refreshResult != null) {
+      refreshResult.executeIfNew(executor);
+    }
+
+    synchronized (lock) {
+      // Immediately resolve the token token if its not expired, or wait for the refresh task to
+      // complete
+      if (getState() != CacheState.EXPIRED) {
+        return Futures.immediateFuture(value);
+      } else if (refreshResult != null) {
+        return refreshResult.task;
+      } else {
+        // Should never happen
+        return Futures.immediateFailedFuture(
+            new IllegalStateException("Credentials expired, but there is no task to refresh"));
       }
     }
   }
 
-  // Must be called under lock
-  private void useAccessToken(AccessToken token, Map<String, List<String>> additionalHeaders) {
-    this.temporaryAccess = token;
-    this.requestMetadata =
-        ImmutableMap.<String, List<String>>builder()
-            .put(
-                AuthHttpConstants.AUTHORIZATION,
-                Collections.singletonList(OAuth2Utils.BEARER_PREFIX + token.getTokenValue()))
-            .putAll(additionalHeaders)
-            .build();
+  /**
+   * Atomically creates a single flight refresh token task.
+   *
+   * <p>Only a single refresh task can be scheduled at a time. If there is an existing task, it will
+   * be returned for subsequent invocations. However if a new task is created, it is the
+   * responsibility of the caller to execute it. The task will clear the single flight slow upon
+   * completion.
+   */
+  private AsyncRefreshResult getOrCreateRefreshTask() {
+    synchronized (lock) {
+      if (refreshTask != null) {
+        return new AsyncRefreshResult(refreshTask, false);
+      }
+
+      final ListenableFutureTask<OAuthValue> task =
+          ListenableFutureTask.create(
+              new Callable<OAuthValue>() {
+                @Override
+                public OAuthValue call() throws Exception {
+                  return OAuthValue.create(refreshAccessToken(), getAdditionalHeaders());
+                }
+              });
+
+      task.addListener(
+          new Runnable() {
+            @Override
+            public void run() {
+              finishRefreshAsync(task);
+            }
+          },
+          MoreExecutors.directExecutor());
+
+      refreshTask = task;
+
+      return new AsyncRefreshResult(refreshTask, true);
+    }
   }
 
-  // Must be called under lock
-  // requestMetadata will never be null if false is returned.
-  private boolean shouldRefresh() {
-    Long expiresIn = getExpiresInMilliseconds();
-    return requestMetadata == null || expiresIn != null && expiresIn <= MINIMUM_TOKEN_MILLISECONDS;
+  /**
+   * Async callback for committing the result from a token refresh.
+   *
+   * <p>The result will be stored, listeners are invoked and the single flight slot is cleared.
+   */
+  private void finishRefreshAsync(ListenableFuture<OAuthValue> finishedTask) {
+    synchronized (lock) {
+      try {
+        this.value = finishedTask.get();
+        for (CredentialsChangedListener listener : changeListeners) {
+          listener.onChanged(this);
+        }
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      } catch (Exception e) {
+        // noop
+      } finally {
+        if (this.refreshTask == finishedTask) {
+          this.refreshTask = null;
+        }
+      }
+    }
+  }
+
+  /**
+   * Unwraps the value from the future.
+   *
+   * <p>Under most circumstances, the underlying future will already be resolved by the
+   * DirectExecutor. In those cases, the error stacktraces will be rooted in the caller's call tree.
+   * However, in some cases when async and sync usage is mixed, it's possible that a blocking call
+   * will await an async future. In those cases, the stacktrace will be orphaned and be rooted in a
+   * thread of whatever executor the async call used. This doesn't affect correctness and is
+   * extremely unlikely.
+   */
+  private static <T> T unwrapDirectFuture(Future<T> future) throws IOException {
+    try {
+      return future.get();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new IOException("Interrupted while asynchronously refreshing the access token", e);
+    } catch (ExecutionException e) {
+      Throwable cause = e.getCause();
+      if (cause instanceof IOException) {
+        throw (IOException) cause;
+      } else if (cause instanceof RuntimeException) {
+        throw (RuntimeException) cause;
+      } else {
+        throw new IOException("Unexpected error refreshing access token", cause);
+      }
+    }
+  }
+
+  /** Computes the effective credential state in relation to the current time. */
+  private CacheState getState() {
+    OAuthValue localValue = value;
+
+    if (localValue == null) {
+      return CacheState.EXPIRED;
+    }
+    Date expirationTime = localValue.temporaryAccess.getExpirationTime();
+
+    if (expirationTime == null) {
+      return CacheState.FRESH;
+    }
+
+    long remainingMillis = expirationTime.getTime() - clock.currentTimeMillis();
+
+    if (remainingMillis <= MINIMUM_TOKEN_MILLISECONDS) {
+      return CacheState.EXPIRED;
+    }
+
+    if (remainingMillis <= REFRESH_MARGIN_MILLISECONDS) {
+      return CacheState.STALE;
+    }
+
+    return CacheState.FRESH;
   }
 
   /**
@@ -231,6 +353,15 @@ public class OAuth2Credentials extends Credentials {
         "OAuth2Credentials instance does not support refreshing the"
             + " access token. An instance with a new access token should be used, or a derived type"
             + " that supports refreshing.");
+  }
+
+  /**
+   * Provide additional headers to return as request metadata.
+   *
+   * @return additional headers
+   */
+  protected Map<String, List<String>> getAdditionalHeaders() {
+    return EMPTY_EXTRA_HEADERS;
   }
 
   /**
@@ -264,21 +395,6 @@ public class OAuth2Credentials extends Credentials {
   }
 
   /**
-   * Return the remaining time the current access token will be valid, or null if there is no token
-   * or expiry information. Must be called under lock.
-   */
-  private Long getExpiresInMilliseconds() {
-    if (temporaryAccess == null) {
-      return null;
-    }
-    Date expirationTime = temporaryAccess.getExpirationTime();
-    if (expirationTime == null) {
-      return null;
-    }
-    return (expirationTime.getTime() - clock.currentTimeMillis());
-  }
-
-  /**
    * Listener for changes to credentials.
    *
    * <p>This is called when token content changes, such as when the access token is refreshed. This
@@ -300,15 +416,29 @@ public class OAuth2Credentials extends Credentials {
 
   @Override
   public int hashCode() {
-    return Objects.hash(requestMetadata, temporaryAccess);
+    return Objects.hashCode(value);
   }
 
+  @Nullable
   protected Map<String, List<String>> getRequestMetadataInternal() {
-    return requestMetadata;
+    OAuthValue localValue = value;
+    if (localValue != null) {
+      return localValue.requestMetadata;
+    }
+    return null;
   }
 
   @Override
   public String toString() {
+    OAuthValue localValue = value;
+
+    Map<String, List<String>> requestMetadata = null;
+    AccessToken temporaryAccess = null;
+
+    if (localValue != null) {
+      requestMetadata = localValue.requestMetadata;
+      temporaryAccess = localValue.temporaryAccess;
+    }
     return MoreObjects.toStringHelper(this)
         .add("requestMetadata", requestMetadata)
         .add("temporaryAccess", temporaryAccess)
@@ -321,13 +451,13 @@ public class OAuth2Credentials extends Credentials {
       return false;
     }
     OAuth2Credentials other = (OAuth2Credentials) obj;
-    return Objects.equals(this.requestMetadata, other.requestMetadata)
-        && Objects.equals(this.temporaryAccess, other.temporaryAccess);
+    return Objects.equals(this.value, other.value);
   }
 
   private void readObject(ObjectInputStream input) throws IOException, ClassNotFoundException {
     input.defaultReadObject();
     clock = Clock.SYSTEM;
+    refreshTask = null;
   }
 
   @SuppressWarnings("unchecked")
@@ -349,6 +479,94 @@ public class OAuth2Credentials extends Credentials {
 
   public Builder toBuilder() {
     return new Builder(this);
+  }
+
+  /** Stores an immutable snapshot of the accesstoken owned by {@link OAuth2Credentials} */
+  static class OAuthValue implements Serializable {
+    private final AccessToken temporaryAccess;
+    private final Map<String, List<String>> requestMetadata;
+
+    static OAuthValue create(AccessToken token, Map<String, List<String>> additionalHeaders) {
+      return new OAuthValue(
+          token,
+          ImmutableMap.<String, List<String>>builder()
+              .put(
+                  AuthHttpConstants.AUTHORIZATION,
+                  ImmutableList.of(OAuth2Utils.BEARER_PREFIX + token.getTokenValue()))
+              .putAll(additionalHeaders)
+              .build());
+    }
+
+    private OAuthValue(AccessToken temporaryAccess, Map<String, List<String>> requestMetadata) {
+      this.temporaryAccess = temporaryAccess;
+      this.requestMetadata = requestMetadata;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof OAuthValue)) {
+        return false;
+      }
+      OAuthValue other = (OAuthValue) obj;
+      return Objects.equals(this.requestMetadata, other.requestMetadata)
+          && Objects.equals(this.temporaryAccess, other.temporaryAccess);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(temporaryAccess, requestMetadata);
+    }
+  }
+
+  enum CacheState {
+    FRESH,
+    STALE,
+    EXPIRED;
+  }
+
+  static class FutureCallbackToMetadataCallbackAdapter implements FutureCallback<OAuthValue> {
+    private final RequestMetadataCallback callback;
+
+    public FutureCallbackToMetadataCallbackAdapter(RequestMetadataCallback callback) {
+      this.callback = callback;
+    }
+
+    @Override
+    public void onSuccess(@Nullable OAuthValue value) {
+      callback.onSuccess(value.requestMetadata);
+    }
+
+    @Override
+    public void onFailure(Throwable throwable) {
+      // refreshAccessToken will be invoked in an executor, so if it fails unwrap the underlying
+      // error
+      if (throwable instanceof ExecutionException) {
+        throwable = throwable.getCause();
+      }
+      callback.onFailure(throwable);
+    }
+  }
+
+  /**
+   * Result from {@link com.google.auth.oauth2.OAuth2Credentials#getOrCreateRefreshTask()}.
+   *
+   * <p>Contains the the refresh task and a flag indicating if the task is newly created. If the
+   * task is newly created, it is the caller's responsibility to execute it.
+   */
+  static class AsyncRefreshResult {
+    private final ListenableFutureTask<OAuthValue> task;
+    private final boolean isNew;
+
+    AsyncRefreshResult(ListenableFutureTask<OAuthValue> task, boolean isNew) {
+      this.task = task;
+      this.isNew = isNew;
+    }
+
+    void executeIfNew(Executor executor) {
+      if (isNew) {
+        executor.execute(task);
+      }
+    }
   }
 
   public static class Builder {
