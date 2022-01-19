5bea0a3d8e35255bfbe249f3a20e8e7d86432765
Neenu1995 <neenushaji@google.com>
2022-01-18 20:05:20 -0500

    chore: generalise script

--- .kokoro/downstream-client-library-check.sh
+++ .kokoro/downstream-client-library-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,52 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Presubmit to ensure the dependencies of the Google Libraries BOM, with the modification of change
-# in the PR, pick up the highest versions among transitive dependencies.
-# https://maven.apache.org/enforcer/enforcer-rules/requireUpperBoundDeps.html
-
 set -eo pipefail
 # Display commands being run.
 set -x
 
 REPO=$1
+CORE_LIBRARY_ARTIFACT=$2
 ## Get the directory of the build script
 scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 ## cd to the parent directory, i.e. the root of the git repo
 cd ${scriptDir}/..
 
-# Make java-core artifacts available for 'mvn validate' at the bottom
+# Make java core library artifacts available for 'mvn validate' at the bottom
 mvn install -DskipTests=true -Dmaven.javadoc.skip=true -Dgcloud.download.skip=true -B -V -q
 
-# Read the current version of this java-core in the POM. Example version: '0.116.1-alpha-SNAPSHOT'
-AUTH_VERSION_POM=pom.xml
-# Namespace (xmlns) prevents xmllint from specifying tag names in XPath
-AUTH_VERSION=`sed -e 's/xmlns=".*"//' ${AUTH_VERSION_POM} | xmllint --xpath '/project/version/text()' -`
-
-if [ -z "${AUTH_VERSION}" ]; then
-  echo "Version is not found in ${AUTH_VERSION_POM}"
-  exit 1
-fi
-echo "Version: ${AUTH_VERSION}"
-
-# Round 1
-# Check this java-auth against HEAD of java-core
-
-git clone "https://github.com/googleapis/java-core.git" --depth=1
-pushd java-core
-
-# replace version
-xmllint --shell <(cat pom.xml) << EOF
-setns x=http://maven.apache.org/POM/4.0.0
-cd .//x:artifactId[text()="google-auth-library-bom"]
-cd ../x:version
-set ${AUTH_VERSION}
-save pom.xml
-EOF
-
-mvn -Denforcer.skip=true clean install
-
-# Read the current version of this java-core in the POM. Example version: '0.116.1-alpha-SNAPSHOT'
+# Read the current version of this java core library in the POM. Example version: '0.116.1-alpha-SNAPSHOT'
 CORE_VERSION_POM=pom.xml
 # Namespace (xmlns) prevents xmllint from specifying tag names in XPath
 CORE_VERSION=`sed -e 's/xmlns=".*"//' ${CORE_VERSION_POM} | xmllint --xpath '/project/version/text()' -`
@@ -69,8 +38,8 @@ if [ -z "${CORE_VERSION}" ]; then
 fi
 echo "Version: ${CORE_VERSION}"
 
-# Round 2
-# Check this java-core against HEAD of java-shared dependencies
+# Round 1
+# Check this java core library against HEAD of java-shared dependencies
 
 git clone "https://github.com/googleapis/java-shared-dependencies.git" --depth=1
 pushd java-shared-dependencies/first-party-dependencies
@@ -78,7 +47,7 @@ pushd java-shared-dependencies/first-party-dependencies
 # replace version
 xmllint --shell <(cat pom.xml) << EOF
 setns x=http://maven.apache.org/POM/4.0.0
-cd .//x:artifactId[text()="google-cloud-core-bom"]
+cd .//x:artifactId[text()="${CORE_LIBRARY_ARTIFACT}"]
 cd ../x:version
 set ${CORE_VERSION}
 save pom.xml
@@ -97,9 +66,9 @@ if [ -z "${SHARED_DEPS_VERSION}" ]; then
   exit 1
 fi
 
-# Round 3
+# Round 2
 
-# Check this BOM against a few java client libraries
+# Check this BOM against java client libraries
 git clone "https://github.com/googleapis/java-${REPO}.git" --depth=1
 pushd java-${REPO}
 
