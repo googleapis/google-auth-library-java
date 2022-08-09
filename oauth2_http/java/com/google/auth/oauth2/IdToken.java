fab2e5a41171702edd93e10232545119e086ee63
Sai Sunder Srinivasan <saisunder@google.com>
2022-08-09 19:56:03 +0000

    fix: remove leading and trailing whitespace from idtoken

--- oauth2_http/java/com/google/auth/oauth2/IdToken.java
+++ oauth2_http/java/com/google/auth/oauth2/IdToken.java
@@ -77,6 +77,7 @@ public class IdToken extends AccessToken implements Serializable {
    * @return returns com.google.auth.oauth2.IdToken
    */
   public static IdToken create(String tokenValue, JsonFactory jsonFactory) throws IOException {
+    tokenValue = tokenValue.strip();
     return new IdToken(tokenValue, JsonWebSignature.parse(jsonFactory, tokenValue));
   }
 
