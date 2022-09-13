669ab042844e46d0503bbd31fd2da92a7963bad5
Sita Lakshmi Sangameswaran <sitalakshmi@google.com>
2022-09-14 04:12:09 +0530

    docs(samples): modified comments in the samples and minor refactor (#990)
    * docs(samples): modified comments in the samples and minor refactor
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * modified comments acc to review
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- samples/snippets/src/main/java/AuthenticateImplicitWithAdc.java
+++ samples/snippets/src/main/java/AuthenticateImplicitWithAdc.java
@@ -16,8 +16,10 @@
 
 // [START auth_cloud_implicit_adc]
 
-import com.google.cloud.compute.v1.Instance;
-import com.google.cloud.compute.v1.InstancesClient;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
 
 public class AuthenticateImplicitWithAdc {
@@ -25,10 +27,11 @@ public class AuthenticateImplicitWithAdc {
   public static void main(String[] args) throws IOException {
     // TODO(Developer):
     //  1. Before running this sample,
-    //  set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+    //  set up Application Default Credentials as described in
+    //  https://cloud.google.com/docs/authentication/external/set-up-adc
     //  2. Replace the project variable below.
-    //  3. Make sure that the user account or service account that you are using
-    //  has the required permissions. For this sample, you must have "compute.instances.list".
+    //  3. Make sure you have the necessary permission to list storage buckets
+    //  "storage.buckets.list"
     String projectId = "your-google-cloud-project-id";
     authenticateImplicitWithAdc(projectId);
   }
@@ -37,24 +40,20 @@ public class AuthenticateImplicitWithAdc {
   // credentials to use.
   public static void authenticateImplicitWithAdc(String project) throws IOException {
 
-    String zone = "us-central1-a";
-    // This snippet demonstrates how to list instances.
     // *NOTE*: Replace the client created below with the client required for your application.
     // Note that the credentials are not specified when constructing the client.
     // Hence, the client library will look for credentials using ADC.
     //
     // Initialize client that will be used to send requests. This client only needs to be created
-    // once, and can be reused for multiple requests. After completing all of your requests, call
-    // the `instancesClient.close()` method on the client to safely
-    // clean up any remaining background resources.
-    try (InstancesClient instancesClient = InstancesClient.create()) {
-      // Set the project and zone to retrieve instances present in the zone.
-      System.out.printf("Listing instances from %s in %s:", project, zone);
-      for (Instance zoneInstance : instancesClient.list(project, zone).iterateAll()) {
-        System.out.println(zoneInstance.getName());
-      }
-      System.out.println("####### Listing instances complete #######");
+    // once, and can be reused for multiple requests.
+    Storage storage = StorageOptions.newBuilder().setProjectId(project).build().getService();
+
+    System.out.println("Buckets:");
+    Page<Bucket> buckets = storage.list();
+    for (Bucket bucket : buckets.iterateAll()) {
+      System.out.println(bucket.toString());
     }
+    System.out.println("Listed all storage buckets.");
   }
 }
 // [END auth_cloud_implicit_adc]
