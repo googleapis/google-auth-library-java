5007743ebf98e411ad1bbcbbb59f6e9b3fc6bd23
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2023-03-15 11:02:44 -0400

    chore: add assertNotSame to test imports

--- oauth2_http/javatests/com/google/auth/oauth2/DefaultCredentialsProviderTest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/DefaultCredentialsProviderTest.java
@@ -34,6 +34,7 @@ package com.google.auth.oauth2;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
