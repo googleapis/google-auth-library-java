238afddc9e9fa5ec86b7574a6ec0ef99c1e381a7
Sai Sunder Srinivasan <saisunder@google.com>
2024-01-10 18:27:08 +0000

    rebase

--- oauth2_http/java/com/google/auth/oauth2/ComputeEngineCredentials.java
+++ oauth2_http/java/com/google/auth/oauth2/ComputeEngineCredentials.java
@@ -139,7 +139,6 @@ public class ComputeEngineCredentials extends GoogleCredentials
             getFromServiceLoader(HttpTransportFactory.class, OAuth2Utils.HTTP_TRANSPORT_FACTORY));
     this.transportFactoryClassName = this.transportFactory.getClass().getName();
 
-    Collection<String> scopes = builder.getScopes();
     // Use defaultScopes only when scopes don't exist.
     Collection<String> scopesToUse = builder.scopes;
     if (scopesToUse == null || scopesToUse.isEmpty()) {
