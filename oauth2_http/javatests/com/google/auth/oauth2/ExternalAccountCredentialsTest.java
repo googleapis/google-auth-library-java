b1096fec9c389a667603beedaa2482cd733d9fa5
Jin <ustc.bigtailwolf@gmail.com>
2024-02-08 14:09:53 -0800

    Revert "Revert "fix: Issue #1347: ExternalAccountCredentials serialization is…" (#1365)
    This reverts commit 5a2d943b3f6f97919b2b1756086537195a72238d.

--- oauth2_http/javatests/com/google/auth/oauth2/ExternalAccountCredentialsTest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/ExternalAccountCredentialsTest.java
@@ -1117,6 +1117,9 @@ public class ExternalAccountCredentialsTest extends BaseSerializationTest {
         testCredentials.getServiceAccountImpersonationOptions().getLifetime(),
         deserializedCredentials.getServiceAccountImpersonationOptions().getLifetime());
     assertSame(deserializedCredentials.clock, Clock.SYSTEM);
+    assertEquals(
+        MockExternalAccountCredentialsTransportFactory.class,
+        deserializedCredentials.toBuilder().getHttpTransportFactory().getClass());
   }
 
   @Test
@@ -1307,6 +1310,11 @@ public class ExternalAccountCredentialsTest extends BaseSerializationTest {
       }
     }
 
+    @Override
+    public Builder toBuilder() {
+      return new Builder(this);
+    }
+
     public static Builder newBuilder() {
       return new Builder();
     }
@@ -1314,9 +1322,17 @@ public class ExternalAccountCredentialsTest extends BaseSerializationTest {
     static class Builder extends ExternalAccountCredentials.Builder {
       Builder() {}
 
+      Builder(TestExternalAccountCredentials credentials) {
+        super(credentials);
+      }
+
       @Override
       public TestExternalAccountCredentials build() {
         return new TestExternalAccountCredentials(this);
+      }
+
+      public HttpTransportFactory getHttpTransportFactory() {
+        return transportFactory;
       }
     }
 
