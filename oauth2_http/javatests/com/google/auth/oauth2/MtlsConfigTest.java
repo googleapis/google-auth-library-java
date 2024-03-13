0f96e8690e83f62cce58df29a410e9f44bd1493e
Riya Mehta <riyamehta@google.com>
2024-03-13 14:28:24 -0700

    plaintext and mtls S2A address.

--- oauth2_http/javatests/com/google/auth/oauth2/MtlsConfigTest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/MtlsConfigTest.java
@@ -1,6 +1,7 @@
 package com.google.auth.oauth2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +10,24 @@ import org.junit.runners.JUnit4;
 /** Test cases for {@link MtlsConfig}. */
 @RunWith(JUnit4.class)
 public class MtlsConfigTest {
-  private static final String S2A_ADDRESS_A = "addr_a";
+  private static final String S2A_PLAINTEXT_ADDRESS = "plaintext";
+  private static final String S2A_MTLS_ADDRESS = "mtls";
 
   @Test
   public void createMtlsConfig_success() {
-    MtlsConfig config = MtlsConfig.createMtlsConfig(S2A_ADDRESS_A);
-    assertEquals(S2A_ADDRESS_A, config.getS2AAddress());
+    MtlsConfig config =
+        MtlsConfig.createBuilder()
+            .setPlaintextS2AAddress(S2A_PLAINTEXT_ADDRESS)
+            .setMtlsS2AAddress(S2A_MTLS_ADDRESS)
+            .build();
+    assertEquals(S2A_PLAINTEXT_ADDRESS, config.getPlaintextS2AAddress());
+    assertEquals(S2A_MTLS_ADDRESS, config.getMtlsS2AAddress());
+  }
+
+  @Test
+  public void createEmptyMtlsConfig_success() {
+    MtlsConfig config = MtlsConfig.createBuilder().build();
+    assertTrue(config.getPlaintextS2AAddress().isEmpty());
+    assertTrue(config.getMtlsS2AAddress().isEmpty());
   }
 }
--- oauth2_http/javatests/com/google/auth/oauth2/S2ATest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/S2ATest.java
@@ -13,43 +13,53 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class S2ATest {
 
-  private static final String S2A_ADDRESS_A = "addr_a";
+  private static final String S2A_PLAINTEXT_ADDRESS = "plaintext";
+  private static final String S2A_MTLS_ADDRESS = "mtls";
 
   @Test
   public void getS2AAddress_validAddress() {
     MockMetadataServerTransportFactory transportFactory = new MockMetadataServerTransportFactory();
-    transportFactory.transport.setS2AAddress(S2A_ADDRESS_A);
+    transportFactory.transport.setPlaintextS2AAddress(S2A_PLAINTEXT_ADDRESS);
+    transportFactory.transport.setMtlsS2AAddress(S2A_MTLS_ADDRESS);
     transportFactory.transport.setRequestStatusCode(HttpStatusCodes.STATUS_CODE_OK);
 
     S2A s2aUtils = new S2A();
     s2aUtils.setHttpTransportFactory(transportFactory);
-    String s2aAddress = s2aUtils.getS2AAddress();
-    assertEquals(S2A_ADDRESS_A, s2aAddress);
+    String plaintextS2AAddress = s2aUtils.getPlaintextS2AAddress();
+    String mtlsS2AAddress = s2aUtils.getMtlsS2AAddress();
+    assertEquals(S2A_PLAINTEXT_ADDRESS, plaintextS2AAddress);
+    assertEquals(S2A_MTLS_ADDRESS, mtlsS2AAddress);
   }
 
   @Test
   public void getS2AAddress_queryEndpointResponseErrorCode_emptyAddress() {
     MockMetadataServerTransportFactory transportFactory = new MockMetadataServerTransportFactory();
-    transportFactory.transport.setS2AAddress(S2A_ADDRESS_A);
+    transportFactory.transport.setPlaintextS2AAddress(S2A_PLAINTEXT_ADDRESS);
+    transportFactory.transport.setMtlsS2AAddress(S2A_MTLS_ADDRESS);
     transportFactory.transport.setRequestStatusCode(
         HttpStatusCodes.STATUS_CODE_SERVICE_UNAVAILABLE);
 
     S2A s2aUtils = new S2A();
     s2aUtils.setHttpTransportFactory(transportFactory);
-    String s2aAddress = s2aUtils.getS2AAddress();
-    assertTrue(s2aAddress.isEmpty());
+    String plaintextS2AAddress = s2aUtils.getPlaintextS2AAddress();
+    String mtlsS2AAddress = s2aUtils.getMtlsS2AAddress();
+    assertTrue(plaintextS2AAddress.isEmpty());
+    assertTrue(mtlsS2AAddress.isEmpty());
   }
 
   @Test
   public void getS2AAddress_queryEndpointResponseEmpty_emptyAddress() {
     MockMetadataServerTransportFactory transportFactory = new MockMetadataServerTransportFactory();
-    transportFactory.transport.setS2AAddress(S2A_ADDRESS_A);
+    transportFactory.transport.setPlaintextS2AAddress(S2A_PLAINTEXT_ADDRESS);
+    transportFactory.transport.setMtlsS2AAddress(S2A_MTLS_ADDRESS);
     transportFactory.transport.setRequestStatusCode(HttpStatusCodes.STATUS_CODE_OK);
     transportFactory.transport.setEmptyContent(true);
 
     S2A s2aUtils = new S2A();
     s2aUtils.setHttpTransportFactory(transportFactory);
-    String s2aAddress = s2aUtils.getS2AAddress();
-    assertTrue(s2aAddress.isEmpty());
+    String plaintextS2AAddress = s2aUtils.getPlaintextS2AAddress();
+    String mtlsS2AAddress = s2aUtils.getMtlsS2AAddress();
+    assertTrue(plaintextS2AAddress.isEmpty());
+    assertTrue(mtlsS2AAddress.isEmpty());
   }
 }
