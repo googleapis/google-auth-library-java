b7cc2fedf5ed0a96c4eeac0897a9e9dc32b18298
aeitzman <eitzman@google.com>
2024-04-11 09:58:34 -0700

    move code into override

--- oauth2_http/java/com/google/auth/oauth2/ExternalAccountCredentials.java
+++ oauth2_http/java/com/google/auth/oauth2/ExternalAccountCredentials.java
@@ -237,14 +237,7 @@ public abstract class ExternalAccountCredentials extends GoogleCredentials {
     this.clientSecret = builder.clientSecret;
 
     if (builder.tokenUrl == null) {
-      try {
-        this.tokenUrl = DEFAULT_TOKEN_URL.replace("{UNIVERSE_DOMAIN}", this.getUniverseDomain());
-      } catch (IOException e) {
-        // Throwing an IOException would be a breaking change, so wrap it here.
-        // This should not happen for this credential type.
-        throw new IllegalStateException(
-            "Error occurred when attempting to retrieve universe domain.", e);
-      }
+      this.tokenUrl = DEFAULT_TOKEN_URL.replace("{UNIVERSE_DOMAIN}", this.getUniverseDomain());
     } else {
       this.tokenUrl = builder.tokenUrl;
     }
@@ -332,6 +325,17 @@ public abstract class ExternalAccountCredentials extends GoogleCredentials {
             callback.onFailure(exception);
           }
         });
+  }
+
+  @Override
+  public String getUniverseDomain() {
+    try {
+      return super.getUniverseDomain();
+    } catch (IOException e) {
+      // Throwing an IOException would be a breaking change, so wrap it here.
+      // This should not happen for this credential type.
+      throw new IllegalStateException(e);
+    }
   }
 
   @Override
