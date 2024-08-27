af1793df85739888212a431c57d6c5881aa64dd1
detmerl <laura.detmer@gmail.com>
2024-08-27 11:38:50 -0400

    add api key credential

---
+++ credentials/java/com/google/auth/ApiKeyCredentials.java
@@ -0,0 +1,45 @@
+package com.google.auth;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ApiKeyCredentials extends Credentials {
+    private static final String API_KEY_HEADER_KEY = "x-goog-api-key";
+    private final String apiKey;
+
+    ApiKeyCredentials(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public static ApiKeyCredentials create(String apiKey) {
+        return new ApiKeyCredentials(apiKey);
+    }
+
+    @Override
+    public String getAuthenticationType() {
+        return "";
+    }
+
+    @Override
+    public Map<String, List<String>> getRequestMetadata(URI uri) throws IOException {
+        return Collections.singletonMap(API_KEY_HEADER_KEY, Collections.singletonList(apiKey));
+    }
+
+    @Override
+    public boolean hasRequestMetadata() {
+        return true;
+    }
+
+    @Override
+    public boolean hasRequestMetadataOnly() {
+        return true;
+    }
+
+    @Override
+    public void refresh() throws IOException {
+
+    }
+}
