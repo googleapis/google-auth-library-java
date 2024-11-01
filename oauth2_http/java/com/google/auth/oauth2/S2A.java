35827ed7a900bdfe78fac27674f8877e1ca0b699
Riya Mehta <riyamehta@google.com>
2024-11-01 12:06:33 -0700

    make S2A class non-final for mocking in tests.

--- oauth2_http/java/com/google/auth/oauth2/S2A.java
+++ oauth2_http/java/com/google/auth/oauth2/S2A.java
@@ -58,7 +58,7 @@ import javax.annotation.concurrent.ThreadSafe;
  * <p>This is an experimental utility.
  */
 @ThreadSafe
-public final class S2A {
+public class S2A {
   static final String S2A_PLAINTEXT_ADDRESS_JSON_KEY = "plaintext_address";
   static final String S2A_MTLS_ADDRESS_JSON_KEY = "mtls_address";
   static final String S2A_CONFIG_ENDPOINT_POSTFIX =
