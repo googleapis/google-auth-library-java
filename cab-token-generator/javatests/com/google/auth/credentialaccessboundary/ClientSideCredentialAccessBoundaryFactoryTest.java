ca338bb5ec6548d8c7c6bbda247bc5a866fd7d05
nbayati <99771966+nbayati@users.noreply.github.com>
2025-01-28 19:45:31 -0800

    Run formatter to resolve lint errors

--- cab-token-generator/javatests/com/google/auth/credentialaccessboundary/ClientSideCredentialAccessBoundaryFactoryTest.java
+++ cab-token-generator/javatests/com/google/auth/credentialaccessboundary/ClientSideCredentialAccessBoundaryFactoryTest.java
@@ -149,7 +149,8 @@ public class ClientSideCredentialAccessBoundaryFactoryTest {
   public void fetchIntermediateCredentials_withCustomUniverseDomain() throws IOException {
     String universeDomain = "foobar";
     GoogleCredentials sourceCredentials =
-        getServiceAccountSourceCredentials(mockTokenServerTransportFactory).toBuilder()
+        getServiceAccountSourceCredentials(mockTokenServerTransportFactory)
+            .toBuilder()
             .setUniverseDomain(universeDomain)
             .build();
 
