f92cc4faf46ab6b0b2b5659fdbbd4c83c1c2f0fe
Min Zhu <zhumin@google.com>
2025-02-03 17:05:30 -0500

    fix: respect token_uri from json in UserCredentials creation. (#1630)
    * fix: respect token_uri from json in UserCredentials creation.
    
    * fix format
    
    * add comment.

--- oauth2_http/java/com/google/auth/oauth2/UserCredentials.java
+++ oauth2_http/java/com/google/auth/oauth2/UserCredentials.java
@@ -124,13 +124,16 @@ public class UserCredentials extends GoogleCredentials implements IdTokenProvide
           "Error reading user credential from JSON, "
               + " expecting 'client_id', 'client_secret' and 'refresh_token'.");
     }
+    // currently "token_uri" is not a default field and needs to be added to json file manually
+    String tokenUrl = (String) json.get("token_uri");
+    URI tokenUri = (tokenUrl == null || tokenUrl.isEmpty()) ? null : URI.create(tokenUrl);
     return UserCredentials.newBuilder()
         .setClientId(clientId)
         .setClientSecret(clientSecret)
         .setRefreshToken(refreshToken)
         .setAccessToken(null)
         .setHttpTransportFactory(transportFactory)
-        .setTokenServerUri(null)
+        .setTokenServerUri(tokenUri)
         .setQuotaProjectId(quotaProjectId)
         .build();
   }
