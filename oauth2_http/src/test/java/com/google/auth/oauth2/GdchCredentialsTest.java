8849f40ea473ecd8fc82606cd777d1080eb2e862
Lawrence Qiu <lawrenceqiu@google.com>
2026-01-06 16:18:44 -0500

    chore: Reset license

--- oauth2_http/src/test/java/com/google/auth/oauth2/GdchCredentialsTest.java
+++ oauth2_http/src/test/java/com/google/auth/oauth2/GdchCredentialsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Google LLC
+ * Copyright 2022, Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
