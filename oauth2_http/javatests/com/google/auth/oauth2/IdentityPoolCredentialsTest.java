3ee6b734e59c7bb82a1a2136806822ac488e01cd
Pranav Iyer <pjiyer@google.com>
2026-04-08 12:28:36 -0700

    Lint fix.

--- oauth2_http/javatests/com/google/auth/oauth2/IdentityPoolCredentialsTest.java
+++ oauth2_http/javatests/com/google/auth/oauth2/IdentityPoolCredentialsTest.java
@@ -40,7 +40,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.GenericJson;
